548580ef5d70bd924edbf50c79fc1e5dfeb2b76e
문정승 <47103479+mjs1995@users.noreply.github.com>
2021-06-04 01:25:31 +0900

    Update 수정본.py

--- visualization/수정본.py
+++ visualization/수정본.py
@@ -82,3 +82,133 @@ ax1.plot(x, x)
 ax2.plot(x, x**2)
 ax3.plot(x, x**3)
 plt.show()
+
+##
+fig, ax = plt.subplots()
+
+ax.plot(x, x**2, "b-")
+ax.plot(px, py, "ro")
+
+ax.text(0, 1.5, "Square function\n$y = x^2$", fontsize=20, color='blue', horizontalalignment="center") # horizontalalignment(수평정렬)
+ax.text(px - 0.08, py, "Beautiful point", ha="right", weight="heavy")
+ax.text(px, py, "x = %0.2f\ny = %0.2f"%(px, py), rotation=50, color='gray')
+
+plt.show()
+
+## 
+fig, ax = plt.subplots()
+ax.plot(x, x**2, px, py, "ro")
+ax.annotate("Beautiful point", xy=(px, py), xytext=(px-1.3,py+0.5),    # 관심있는 부분의 위치를 지정하고, 텍스트의 위치를 지정 및 화살표 추가속성
+                           color="green", weight="heavy", fontsize=14,
+                           arrowprops={"facecolor": "lightgreen"})
+plt.show()
+
+fig, ax = plt.subplots()
+ax.plot(x, x**2)
+ax.plot(px, py, "ro")
+
+bbox_props = dict(boxstyle="rarrow,pad=0.3", ec="b", lw=2, fc="lightblue") 
+ax.text(px-0.2, py, "Beautiful point", bbox=bbox_props, ha="right")
+
+bbox_props = dict(boxstyle="round4,pad=1,rounding_size=0.2", ec="black", fc="#EEEEFF", lw=5)
+ax.text(0, 1.5, "Square function\n$y = x^2$", fontsize=20, color='black', ha="center", bbox=bbox_props)
+
+plt.show()
+
+## 범례
+x = np.linspace(-1.4, 1.4, 50)
+
+fig, ax = plt.subplots()
+
+ax.plot(x, x**2, "r--", label="Square function")
+ax.plot(x, x**3, "g-", label="Cube function")
+ax.legend(loc="best")
+ax.grid(True)
+plt.show()
+
+
+## scatter
+fig, ax = plt.subplots()
+
+for color in ['red', 'green', 'blue']:
+    n = 100
+    x, y = rand(2, n)
+    scale = 500.0 * rand(n) ** 5
+    ax.scatter(x, y, s=scale, c=color, alpha=0.3, edgecolors='blue')
+
+ax.grid(True)
+
+plt.show()
+
+## 일반
+from numpy.random import randn
+
+# Axis를 인자로 전달하여 함수 연산과 시각화를 수행합니다.
+def plot_line(axis, slope, intercept, **kargs):
+    xmin, xmax = axis.get_xlim()
+    axis.plot([xmin, xmax], [xmin*slope+intercept, xmax*slope+intercept], **kargs)
+
+x = randn(1000)
+y = 0.5*x + 5 + randn(1000)*2
+
+fig, ax = plt.subplots()
+
+ax.set_xlim(-2.5, 2.5)
+ax.set_ylim(-5, 15)
+ax.scatter(x, y, alpha=0.2)
+ax.plot(1, 0, "ro")
+ax.vlines(1, -5, 0, color="red")
+ax.hlines(0, -2.5, 1, color="red")
+plot_line(axis=ax, slope=0.5, intercept=5, color="magenta")  
+ax.grid(True)
+plt.show()
+
+## 히스토그램
+data1 = np.random.randn(400)
+data2 = np.random.randn(500) + 3
+data3 = np.random.randn(450) + 6
+data4a = np.random.randn(200) + 9
+data4b = np.random.randn(100) + 10
+
+fig, ax = plt.subplots()
+ax.hist(data1, bins=5, color='g', alpha=0.75, label='bar hist') # default histtype='bar'
+ax.hist(data2, color='b', alpha=0.65, histtype='stepfilled', label='stepfilled hist')
+ax.hist(data3, color='r', histtype='step', label='step hist')
+ax.hist((data4a, data4b), color=('r','m'), alpha=0.55, histtype='barstacked', label=('barstacked a', 'barstacked b'))
+
+ax.set_xlabel("Value")
+ax.set_ylabel("Frequency")
+ax.legend()
+ax.grid(True)
+plt.show()
+
+## 애니메이션
+import matplotlib.animation as animation
+matplotlib.rc('animation', html='jshtml')
+
+x = np.linspace(-1, 1, 100)
+y = np.sin(x**2*25)
+data = np.array([x, y])
+
+fig, ax = plt.subplots()
+
+line, = ax.plot([], [], "r-") # start with an empty plot
+ax.set_xlim(-1.1, 1.1)
+ax.set_ylim(-1.1, 1.1)
+ax.plot([-0.5, 0.5], [0, 0], "b-", [0, 0], [-0.5, 0.5], "b-", 0, 0, "ro")
+ax.grid(True)
+ax.set_title("Marvelous animation")
+
+# this function will be called at every iteration
+def update_line(num, data, line):
+    line.set_data(data[..., :num] + np.random.rand(2, num) / 25)  # we only plot the first `num` data points.
+    return line,
+
+line_ani = animation.FuncAnimation(fig, update_line, frames=100, fargs=(data, line), interval=67)
+plt.close()
+line_ani
+
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+line_ani.save('my_wiggly_animation.mp4', writer=writer)
+
